dc840a9552832b610af68738849e0f9476530364
Flaryiest <ericmzuo1@gmail.com>
2024-02-11 10:05:05 -0700

    created header

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
 const box = document.querySelector("div");
+const newGrid = document.querySelector(".newGrid");
+newGrid.addEventListener("click", function()  {createGrid()})
 
+function createGrid() {
+    let squares = prompt("Enter # of squares per row (Limit 100)")
+}
 
